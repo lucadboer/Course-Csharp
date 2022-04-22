5cdc17f072825752900a63846c4b56d075f3d38b
lucadboer <97457092+lucadboer@users.noreply.github.com>
2022-04-22 12:07:42 -0300

    add files

--- Herança/Program.cs
+++ Herança/Program.cs
@@ -9,6 +9,8 @@ namespace Course
 		{
 			BusinessAcount count = new BusinessAcount(40, "Helo", 800.70, 10000);
             Console.WriteLine(count.Balance);
+
+			//count.Balance = 800.41;
 		}
 	}
 }
